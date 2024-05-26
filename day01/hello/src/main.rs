a0afb768ffd8d1af2d1a86c6cc3c13575bb8a386
foxxcn <foxxcnweb@gmail.com>
2024-05-26 11:57:23 +0800

    cop this

--- day01/hello/src/main.rs
+++ day01/hello/src/main.rs
@@ -1,25 +1,65 @@
+use hello::ThreadPool;
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
+// --snip--
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    // --snip--
 
-    println!("Request: {:#?}", http_request);
+    let buf_reader = BufReader::new(&mut stream);
+    let request_line = match buf_reader.lines().next() {
+        Some(Ok(line)) => line,
+        Some(Err(e)) => {
+            eprintln!("Failed to read line: {}", e);
+            return;
+        }
+        None => {
+            eprintln!("Client disconnected before sending any data");
+            return;
+        }
+    };
+
+    let (status_line, filename) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    };
+
+    // --snip--
+
+    let contents = match fs::read_to_string(filename) {
+        Ok(contents) => contents,
+        Err(e) => {
+            eprintln!("Failed to read file {}: {}", filename, e);
+            return;
+        }
+    };
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    if let Err(e) = stream.write_all(response.as_bytes()) {
+        eprintln!("Failed to send response: {}", e);
+    }
 }
